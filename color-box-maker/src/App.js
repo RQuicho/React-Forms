61c6081466991bad149b96d245b3fce88fef8823
RQuicho <raymondquicho@gmail.com>
2023-10-08 18:29:56 -0700

    adding code for Box component

--- color-box-maker/src/App.js
+++ color-box-maker/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Box from './Box';
+import BoxList from './BoxList';
 
 function App() {
   return (
     <div className="App">
-    
+      {/* <BoxList /> */}
+      <Box color='red' width={150} height={150}/>
+      <Box color='blue' width={150} height={150}/>
+
     </div>
   );
 }
---
+++ color-box-maker/src/Box.js
@@ -0,0 +1,22 @@
+import {React, useState} from 'react';
+
+const Box = ({color, width, height}) => {
+  const [isVisible, setIsVisible] = useState(true);
+  const remove = () => {
+    setIsVisible(false);
+  }
+  return (
+    <>
+    {isVisible && 
+      <div style={{backgroundColor: color, width: width, height: height}}>
+        <p>Box</p>
+      </div>  
+    }
+    {isVisible &&
+      <button onClick={remove}>X</button>
+    }
+    </>
+  )
+}
+
+export default Box;
